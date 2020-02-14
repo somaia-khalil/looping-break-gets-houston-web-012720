79064a9cc0a1521003fa066d297054ba397cb039
Somaia Khalil <sumaya_khair@live.com>
2020-02-14 04:36:12 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -9,13 +9,3 @@ def levitation_quiz
 	puts 'You passed the quiz!'
 end
 
-
-def levitation_quiz
-	#your code here
-	loop do 
-		puts "What is the spell that enacts levitation?"
-		answer = gets.chomp
-		break if answer == "Wingardium Leviosa"
-	end
-	puts "You passed the quiz!"
-end
