0af5eccd3de8ed03aef0bbaf478afb6cf2bf6476
Somaia Khalil <sumaya_khair@live.com>
2020-02-14 04:30:15 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -5,9 +5,9 @@ def levitation_quiz
 	  
 	  puts 'What is the spell that enacts levitation?'
 	  answer = get.chomp 
-	 break  if answer == 
-	  
+	 break  if answer == "Wingardium Leviosa"
 	end 
+	
 end
 
 
